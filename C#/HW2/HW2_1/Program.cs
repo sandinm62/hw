be10cb036c44358953741b1cf208627796d64f4e
Michael <sandinm62@live.com>
2023-04-11 22:40:49 +0300

    Added script Task 2_1

--- C#/HW2/HW2_1/Program.cs
+++ C#/HW2/HW2_1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number > 99 && number < 1000)   // проверка того что чило трёхзначное
+    {
+        int count = number % 100;       // получаем остаток от деления на сотни
+        count = count / 10;             // получаем результат деления не десятки
+        Console.WriteLine(count);
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели не трёхзначное число");
+    }
+
