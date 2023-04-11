ebab83de26787c2cecd6ca3ec57736d15c9dbaf2
Michael <sandinm62@live.com>
2023-04-11 22:56:10 +0300

    Added no finish script HW2_2

--- C#/HW2/HW2_2/Program.cs
+++ C#/HW2/HW2_2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[];
+
+if (number > 99)
+{
+    int count = 0;
+    position = number;
+    while (count < position)
+    {
+     //   array[count] = position;
+    }
+
+}
+
