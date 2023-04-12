0f88b66f9c2a7deb2893cc2d0ccaba06e2dc3aea
sandinm62 <sandinm62@live.com>
2023-04-12 09:45:47 +0300

    Added worked script and comment Task 2_2

--- C#/HW2/HW2_2/Program.cs
+++ C#/HW2/HW2_2/Program.cs
@@ -6,16 +6,37 @@
 
 Console.Write("Enter number: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[];
-
+                
+int numberCopy = number;
+int count = 0;
 if (number > 99)
 {
-    int count = 0;
-    position = number;
-    while (count < position)
+                    
+                    
+                    
+    while (number > 0)                              //считаем количество чисел
     {
-     //   array[count] = position;
+                        
+        number = number / 10;
+        count++;
+                        
+                        
     }
 
 }
+                
+    Console.WriteLine(count);
+
+        int[] array = new int[count];               // создаём массив размер которого равен количеству цыфр в числе
+        int countArray = 0;
+        while (numberCopy > 0)                      
+        {
+            array[countArray] = numberCopy % 10;    //присваиваем порядковому элементу массива остаток от числа при делении на 10
+            numberCopy /= 10;                       // делим число на 10 чтобы убрать остаток
+                    
+                    
+            countArray++;
+        }
+        
+Console.Write(array[array.Length - 3] );            // т.к. в массиве у нас число перевёрнуто выводим третий элемент с конца который у нас в оригинале в числа стоит вначале 
 
