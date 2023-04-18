a6c4bfb82d0cac74f1b2aab471a0c20152461c36
sandinm62 <sandinm62@live.com>
2023-04-18 14:07:55 +0300

    Added comments Task 3_1

--- C#/HW3/HW3_1/Program.cs
+++ C#/HW3/HW3_1/Program.cs
@@ -6,20 +6,20 @@
 
 Console.Write("Enter number: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[5];
+int[] array = new int[5];                                           //массив из пяти ячеек чтобы записать туда остатки от деления на 10
 int count = 0;
-int divider = 10;
-if (number > 9999 && number < 100000)
+int divider = 10;                                                   //Обозначил переменную делитель чтобы не было магических чисел в коде
+if (number > 9999 && number < 100000)                               //Проверяем что в числе пять знаков
 {
     while (count < array.Length)
     {
-        array[count] = number % divider;
+        array[count] = number % divider;                            //присваем остаток порядковому элементу массива
         number = number / divider;
         
         count++;
     }
 
-    if (array[0] == array[4] && (array[1] == array[3]))
+    if (array[0] == array[4] && (array[1] == array[3]))             //сравниваем элеменнты массива чтобы вычислить равенство чисел
     {
         Console.WriteLine("Полиндром ");
     }
