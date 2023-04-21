d372f9b3808f0e745d4ac0d2a5bb5e020cf896ed
HomaLT <sandinm62@live.com>
2023-04-21 10:45:53 +0300

    Added script Task4_2

---
+++ C#/HW4/HW4_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int sum(int numberSum)
+{
+    int result = 0;
+
+    while (numberSum > 0)
+    {
+        result = result + (numberSum % 10);
+        numberSum = numberSum / 10;
+    }
+
+    return result;
+}
+
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(sum(number));
+
