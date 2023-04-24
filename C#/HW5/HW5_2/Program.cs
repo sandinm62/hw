3c5ea003476be266acd8ca82af6a7668fae5a3dd
HomaLT <sandinm62@live.com>
2023-04-24 16:54:07 +0300

    Added script Task5_2

---
+++ C#/HW5/HW5_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] devArray(int[] array1)
+{
+    for (int count = 0; count < array1.Length; count++)
+    {
+        array1[count] = new Random().Next(-100, 101);
+    }
+
+    return array1;
+}
+
+
+
+
+
+int array = new int[4];
+
+devArray(array);
