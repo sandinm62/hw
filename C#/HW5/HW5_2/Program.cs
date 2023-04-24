5e8fc09cdf1fb8ad9abb6d9175019108d142ef3f
HomaLT <sandinm62@live.com>
2023-04-24 17:08:30 +0300

    Added method summ Task5_2

--- C#/HW5/HW5_2/Program.cs
+++ C#/HW5/HW5_2/Program.cs
@@ -3,7 +3,7 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-int[] devArray(int[] array1)
+int[] devArray(int[] array1)                            //Метод присваивания случайных значений
 {
     for (int count = 0; count < array1.Length; count++)
     {
@@ -13,10 +13,26 @@ int[] devArray(int[] array1)
     return array1;
 }
 
+int summEven(int[] array2)                              //Метод подсчёта суммы чётных позиций массива
+{
+    int summ = 0;
+    for (int count = 0; count < array2.Length; count++)
+    {
+        if (count % 2 == 1)
+        {
+            summ = summ + array2[count];
+        }
+    }
+
+
+    return summ;
+}
 
 
 
-
-int array = new int[4];
+int[] array = new int[4];
 
 devArray(array);
+Console.WriteLine(String.Join(" ", array));
+
+Console.WriteLine(summEven(array));
