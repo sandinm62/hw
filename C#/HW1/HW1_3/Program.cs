36ab1788720b3fbb6a7cd277cd297bc6fdedd268
sandinm62 <sandinm62@live.com>
2023-06-13 15:20:10 +0300

    Fixed ferst HW1

---
+++ C#/HW1/HW1_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(number % 2 == 0 ? "Even" : "No even");
+
